485ad34e4a4a608ce5a3eeea55389650fad76e9c
schmuck <schmuck@schmuck-desktop>
2016-11-12 12:49:45 +0100

    kleime Verbesserung

--- JKlubTV Frontend/receiveJSON.php
+++ JKlubTV Frontend/receiveJSON.php
@@ -5,16 +5,7 @@ if (! empty ( $_POST )) {
 	$jsonFileName = $_POST ["jsonFileName"];
 	$menuName = $_POST ["menuName"];
 	$configFlag = $_POST ["configFlag"];
-	// $script = "";
-	// if ($configFlag == "true") {
-	// Datei neu erstellen
-	// unlink ( $configfile );
-	// array_map ( 'unlink', glob ( "tables/*.html" ) );
-	// array_map ( 'unlink', glob ( "jsonFiles/*.json" ) );
-	// $script .= '<?php' . "\n";
-	// $script .= ' $menuName = "' . $menuName . '";' . "\n";
-	// $script .= ' $jsonFiles[] = array();' . "\n";
-	// }
+	
 	$file = htmlspecialchars ( 'jsonFiles/' . $jsonFileName );
 	$bodytag = html_entity_decode ( $string, ENT_QUOTES );
 	file_put_contents ( $file, $bodytag );
