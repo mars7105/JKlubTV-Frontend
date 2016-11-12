e2b6302ae8a9b0b7be3c34d19290b054eee97be7
M. Schmuck <m_schmuck@gmx.net>
2016-11-12 08:59:54 +0100

    Delete receiveFileJSON.php

--- receiveFileJSON.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (! empty ( $_POST )) {
-	$configfile = "config.json";
-	$string = $_POST ["jsonFiles"];
-	$bodytag = html_entity_decode ( $string, ENT_QUOTES );
-	
-	file_put_contents ( $configfile, $bodytag );
-}
-?>
