0c7e7a5c3854152ce71849d60d8b4001a756955c
schmuck <schmuck@schmuck-desktop>
2016-12-02 18:25:44 +0100

    ...

--- JKlubTV Frontend/lib/getcontent.php
+++ JKlubTV Frontend/lib/getcontent.php
@@ -14,18 +14,14 @@ function showGroupTable($index) {
 	$jsonArray = json_decode ( $json, true );
 	$jsonFiles = $jsonArray ['htmlfiles'] [0];
 	
-	// $jsonFiles = json_decode ( $json );
+	
 	
 	$filename = $jsonFiles [$index];
 	if (file_exists ( $filename )) {
-		// liest den Inhalt einer Datei in einen String
-		// $handle = fopen ( $filename, "r" );
-		// $file = fread ( $handle, filesize ( $filename ) );
-		// fclose ( $handle );
+		
 		include $filename;
 	}
 	
-	// return $file;
 }
 
 ?>
